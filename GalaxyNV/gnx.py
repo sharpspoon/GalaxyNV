ebfabd8408f1873e3c96c7aa3748cda12f39ae10
sharpspoon <robin@steelcitysites.net>
2021-02-15 14:24:25 -0600

    force work

--- GalaxyNV/gnx.py
+++ GalaxyNV/gnx.py
@@ -2,7 +2,16 @@ from datetime import datetime
 from flask import render_template
 from GalaxyNV import app
 import networkx as nx
+import matplotlib.pyplot as plt
+
+def a():
+    G = nx.petersen_graph()
+    #plt.subplot(121)
+    #plt.show()
+    nx.draw(G, with_labels=True, font_weight='bold')
 
 def b():
-  # Something
-  print ('asdf')
+    G = nx.graph
+    G.add_node(1)
+    G.add_nodes_from([2, 3])
+    return (G)
