ed810f4605a2e186866d04274f5d0d92ac7c9f19
sharpspoon <robin@steelcitysites.net>
2021-02-15 11:14:21 -0600

    test data work

--- GalaxyNV/views.py
+++ GalaxyNV/views.py
@@ -4,16 +4,20 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from GalaxyNV import app
+from GalaxyNV import app, gnx
+
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
+    a = gnx.b
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
+        test=a
     )
 
 @app.route('/contact')
