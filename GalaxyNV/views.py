6bc7e47627ce6fa898d665b04add40b0eae76706
Robin Ward <robin.ward01@sap.com>
2021-03-29 15:58:51 -0500

    iframe issue fixed

--- GalaxyNV/views.py
+++ GalaxyNV/views.py
@@ -3,19 +3,6 @@ from flask import Flask, request, render_template, send_file
 from GalaxyNV import app, gnx, networkfiles
 import yaml
 
-
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
-
 @app.route('/')
 @app.route('/home')
 def home():
