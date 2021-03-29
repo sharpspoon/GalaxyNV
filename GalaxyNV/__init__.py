85953ff3b60c205b0f7082009f244a920ae383e3
Robin Ward <robin.ward01@sap.com>
2021-03-29 15:58:51 -0500

    iframe issue fixed

--- GalaxyNV/__init__.py
+++ GalaxyNV/__init__.py
@@ -9,6 +9,9 @@ import GalaxyNV.views
 import GalaxyNV.gnx
 import GalaxyNV.networkfiles
 
+#This fixes the iframe loading issue
+app.templates_auto_reload=True
+
 networkfiles.createfolders()
 networkfiles.loadfiles()
 networkfiles.convert()
