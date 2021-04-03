c0a3d372687e22a58c58ca48c1d485cefeb3f696
Robin Ward <robin.ward01@sap.com>
2021-04-03 12:12:41 -0500

    delete node checkbox updated

--- GalaxyNV/networkfiles.py
+++ GalaxyNV/networkfiles.py
@@ -256,7 +256,7 @@ def load_nodes_to_edit():
                 for l in data1["nodes"][n]["links"]:
                     links+=(r'''<table><tr><td><select class="form-select" aria-label="Default select example"><option selected>'''+str(l)+'''</option>'''+link_list+'''</select></td><td><input type="checkbox" class="btn-check" id="btn-check_'''+n+'''_'''+str(l)+'''" autocomplete="off"><label class="btn btn-outline-danger" for="btn-check_'''+n+'''_'''+str(l)+'''">X</label></td></tr></table>''')
                 
-                nodes+=(r'''<tr><th scope="row"><input type="text" class="form-control" id="nodeNameId" name="'''+n+'''" aria-describedby="nodeHelp" value="'''+str(n)+r'''" required></th><td>'''+str(links)+r'''</td><td><input type="number" class="form-control" id="numberOfNodesId" name="replicas_'''+str(n)+'''" value="'''+str(replicas)+'''"></td><td><input type="checkbox" class="form-check-input" id="id_delete_'''+n+'''" name="delete_'''+n+'''"></td></tr>''')
+                nodes+=(r'''<tr><th scope="row"><input type="text" class="form-control" id="nodeNameId" name="'''+n+'''" aria-describedby="nodeHelp" value="'''+str(n)+r'''" required></th><td>'''+str(links)+r'''</td><td><input type="number" class="form-control" id="numberOfNodesId" name="replicas_'''+str(n)+'''" value="'''+str(replicas)+'''"></td><td><input type="checkbox" class="btn-check form-control" id="id_delete_'''+n+'''" name="delete_'''+n+'''" autocomplete="off""><label class="btn btn-outline-danger" for="id_delete_'''+n+'''">X</label></td></tr>''')
             return nodes
     except:
         return ("Failed to open network.json file. Are you sure it is named correctly?")
